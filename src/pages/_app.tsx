eaa346bc41a91eee002f1ed23a00894f839b6ebd
h3zord <lucaschmain@gmail.com>
2023-10-02 11:51:53 -0300

    feat: bomb commit

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,27 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { globalStyles } from '@/styles/global'
+import { Roboto } from 'next/font/google'
+import { Container, Header } from '../styles/pages/app'
+import Image from 'next/image'
+import logoImg from '../assets/logo.svg'
+
+const roboto = Roboto({
+  weight: ['400', '700'],
+  style: ['normal'],
+  subsets: ['latin'],
+})
+
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={roboto.className}>
+      <Container>
+        <Header>
+          <Image src={logoImg.src} width="130" height="52" alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </main>
+  )
 }
